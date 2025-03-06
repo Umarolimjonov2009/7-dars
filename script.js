0f4f8bbb3d8ea1cbcb36eacbda7076c979d2a6a6
Umarolimjonov2009 <feruzashokirova1988@gmail.com>
2025-03-06 17:27:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+
+
+
+const raqamlar=[96,4568,25,87,48,615,8,213,99,12,98,23,98,13,812,9,5];
+let juft=[];
+
+
+
+for(let i=0; i<raqamlar.length; i++){
+    if(raqamlar[i]%2==0){
+        juft.push(raqamlar[i])
+    }
+}
+
+
+console.log("Boshlanishdagi array",raqamlar);
+console.log("Juft sondagi array",raqamlar);
+
